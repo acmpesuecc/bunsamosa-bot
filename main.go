28fa5de1b49afb613c2cbaa9a41b65d57cbaa0a3
DedLad <adheshathrey2004@gmail.com>
2024-10-02 13:02:19 +0530

    refactor: explicit method for endpoints in net/http

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
-	//"os"
+	"os"
 
 	// "github.com/go-playground/webhooks/v6"
 
-	//"github.com/anirudhRowjee/bunsamosa-bot/globals"
+	"github.com/anirudhRowjee/bunsamosa-bot/globals"
 	"github.com/anirudhRowjee/bunsamosa-bot/handlers"
 	"github.com/rs/cors"
 )
@@ -20,30 +20,30 @@ func main() {
 	// Initialize state
 	// TODO Separate the YAML Loading from the value setting
 	// var YAML_SECRETS_PATH string
-	// YAML_SECRETS_PATH := ""
-	//
-	// // Check if we're in a development environment
-	// IS_DEV_ENV := os.Getenv("BUNSAMOSA_DEV_MODE")
-	//
-	// if IS_DEV_ENV == "1" {
-	// 	YAML_SECRETS_PATH = "./secrets-dev.yaml"
-	// } else {
-	// 	YAML_SECRETS_PATH = "/root/bunsamosa-bot/secrets.yaml"
-	// }
-	//
-	// globals.Myapp = globals.App{}
-	//
-	// globals.Myapp.Parse_from_YAML(YAML_SECRETS_PATH)
-	// log.Println("[INIT] YAML Parsed successfully")
-	//
-	// // Initialize the Github Client
-	// globals.Myapp.Initialize_github_client()
-	// // Initialize the database
-	// globals.Myapp.Initialize_database()
-	//
-	// // Serve!
-	// // TODO use Higher-Order Functions to generate this response function
-	// // with the webhook secret from the YAML Parsed into the app in scope
+	YAML_SECRETS_PATH := ""
+
+	// Check if we're in a development environment
+	IS_DEV_ENV := os.Getenv("BUNSAMOSA_DEV_MODE")
+
+	if IS_DEV_ENV == "1" {
+		YAML_SECRETS_PATH = "./secrets-dev.yaml"
+	} else {
+		YAML_SECRETS_PATH = "/root/bunsamosa-bot/secrets.yaml"
+	}
+
+	globals.Myapp = globals.App{}
+
+	globals.Myapp.Parse_from_YAML(YAML_SECRETS_PATH)
+	log.Println("[INIT] YAML Parsed successfully")
+
+	// Initialize the Github Client
+	globals.Myapp.Initialize_github_client()
+	// Initialize the database
+	globals.Myapp.Initialize_database()
+
+	// Serve!
+	// TODO use Higher-Order Functions to generate this response function
+	// with the webhook secret from the YAML Parsed into the app in scope
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /Github", handlers.WebhookHandler)
