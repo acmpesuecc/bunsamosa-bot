469011f9e4c7773ab8199faeb62be71bb769bcba
Anirudh Sudhir <137381184+anirudhsudhir@users.noreply.github.com>
2025-08-17 14:23:03 +0530

    chore: bump dependencies and add Dev guide
    Co-authored-by: Aditya Hegde <burrawta@gmail.com>
    Co-authored-by: Delta18-Git <64347927+Delta18-Git@users.noreply.github.com>
    Signed-off-by: Anirudh Sudhir <137381184+anirudhsudhir@users.noreply.github.com>

---
+++ dev/init.sql
@@ -0,0 +1,11 @@
+INSERT INTO maintainer_models ('username')
+VALUES ('anirudhsudhir'),
+('bwaklog'),
+('Delta18-Git'),
+('Dedlad');
+
+INSERT INTO contributors ('github_handle')
+VALUES ('@anirudhsudhir'),
+('@bwaklog'),
+('@Delta18-Git'),
+('@Dedlad');
